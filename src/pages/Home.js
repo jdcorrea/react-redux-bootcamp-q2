70ce1e4b270f2b72bf9def55712531256941dd03
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2022-07-14 17:45:39 -0500

    serverless using netlify-lambdas test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,15 @@ import {Main} from '../styles/pages/Home.styles';
 export const Home = () => {
   return (
     <Main>
+      <button
+        onClick={() => {
+          fetch('/.netlify/functions/hello')
+          .then(res => res.json())
+          .then(({message}) => console.log('mensaje', {message}))
+        }}
+      >
+        Testing
+      </button>
       <h1>Welcome to WizeStore!</h1>
       <p>
         Browse our <Link to='/products'>products</Link>
