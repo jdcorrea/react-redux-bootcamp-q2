6876662bd669a6075e970f9c2d3a2ed3acb86441
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2022-07-14 18:57:47 -0500

    testing serverless netlify

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,8 @@
 exports.handler = function(event, context, callback) {
-  return {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       message: 'hello mdfk'
     }) 
-  }
+  })
 }
